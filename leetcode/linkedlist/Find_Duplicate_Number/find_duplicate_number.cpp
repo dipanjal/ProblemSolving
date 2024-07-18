99ca5a6f2a2a447e30b1583a5b33957ed1898627
dipanjal <dipanjalmaitra@gmail.com>
2024-07-18 16:39:46 +0600

    LC-287: c++ solution added

---
+++ leetcode/linkedlist/Find_Duplicate_Number/find_duplicate_number.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+        while (true) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if (slow == fast) {
+                break;
+            }
+        }
+        int slow2 = nums[0];
+        while (slow != slow2) {
+            slow = nums[slow];
+            slow2 = nums[slow2];
+        }
+        return slow;
+    }
+};

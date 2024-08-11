961a27cce8c753cf27c38295bac457ae9c256250
dipanjal <dipanjalmaitra@gmail.com>
2024-08-11 13:26:01 +0600

    Remove Duplicate from Sorted List

---
+++ leetcode/linkedlist/Remove_Duplicate_from_Sorted_List/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    private static ListNode {
+        int val;
+        ListNode next;
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode curr = head;
+        while (curr != null && curr.next != null) {
+            if (curr.val == curr.next.val) {
+                curr.next = curr.next.next;
+            } else {
+                curr = curr.next;
+            }
+        }
+        return head;
+    }
+}

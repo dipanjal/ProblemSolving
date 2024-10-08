84f6595cf2a6bfc1cbcbd21d21eb99ba53b6afcd
dipanjal <dipanjalmaitra@gmail.com>
2024-10-08 16:47:20 +0600

    LC-509: executor script added to run diff solutions

---
+++ leetcode/dp/Fibbonacci_Number/python/executor.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Fibbonacci Number: recursive_solution.py"
+python ./recursive_solution.py
+echo "Fibbonacci Number: memoization_recursion.py"
+python ./memoization_recursion.py
+echo "Fibbonacci Number: tabulation.py"
+python ./tabulation.py
+echo "Fibbonacci Number: tabulation_optimized.py"
+python ./tabulation_optimized.py

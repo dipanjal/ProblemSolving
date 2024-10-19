4f2d0da4063da4c598545b6f5381edf84e598915
dipanjal <dipanjalmaitra@gmail.com>
2024-10-20 00:18:04 +0600

    recursion: count no of subsequences where sum=k

---
+++ recursion/9_count_subsequence_where_sum_is_k .py
@@ -0,0 +1,35 @@
+def countNumOfSubsequence(nums: list[int], k: int):
+    def count(i, sub: list[int], sum: int):
+        # all numbers are positive, so we can say when the sum is already grater that K
+        # that subsequence can never be the appropriate one
+        # so it's better to terminate it
+        if sum > k:
+            return 0
+        
+        if i >= len(nums):
+            if sum == k:
+                print(f"sub: {sub} | sum: {sum}")
+                return 1
+            return 0
+        
+        # pick current number
+        sub.append(nums[i])
+        # add current number with sum
+        sum += nums[i]
+        # recurse
+        l_count = count(i + 1, sub, sum)
+
+        # or not pick the item | remove the current number from the subsequence
+        sub.remove(nums[i])
+        # subtract current number from the sum
+        sum -= nums[i]
+        # recurse
+        r_count = count(i + 1, sub, sum)
+
+        return l_count + r_count
+    
+    return count(0, [], 0)
+
+
+if __name__ == "__main__":
+    print(f"No of Subsequences: {countNumOfSubsequence(nums=[1, 2, 1], k=2)}")

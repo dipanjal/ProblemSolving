6549645f9663526599142802662f6b4693a4f97f
dipanjal <dipanjalmaitra@gmail.com>
2024-10-21 09:45:50 +0600

    perf util added

--- leetcode/dp/House_Robber/utils.py
+++ leetcode/dp/House_Robber/utils.py
@@ -4,6 +4,8 @@ from functools import wraps
 def log_execution_time(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
+        # classified_function_name = f"{args[0].__class__.__name__}.{func.__name__}"
+        print(f"-------- Executing {func.__name__} --------")
         start = timeit.default_timer()
         result = func(*args, **kwargs)
         elapsed_time_ms = (timeit.default_timer() - start) * 1000
@@ -18,7 +20,7 @@ def log_execution_time(func):
                     return f"{elapsed_time_sec:.2f} seconds"
             else:
                 return f"{elapsed_time_ms:.3f} ms"
-
+        
         formatted_time = format_execution_time(elapsed_time_ms)
         print(f"{func.__name__} executed in {formatted_time}")
         return result

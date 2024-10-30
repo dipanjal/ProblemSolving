f24fd87c2026c77c5bc13872743d973363531ade
dipanjal <dipanjalmaitra@gmail.com>
2024-10-30 13:14:42 +0600

    LC-70: Climbing Stair (Java tabulation soln)

---
+++ leetcode/dp/Climbing_Stairs/java/SolutionTebulation.java
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/climbing-stairs/description/
+// 70. Climbing Stairs
+
+// Tabulation solution | Bottom-Up | Dynamic Programming
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+class SolutionTabulation {
+    public int climbStairs(int n) {
+        // tabulation array to store the results of the subproblems
+        int ways[] = new int[n+1];
+        // base cases
+        ways[1] = 1;
+        ways[2] = 2;
+        for (int i=3; i<n+1; i++) {
+            ways[i] = ways[i-2] + ways[i-1];
+        }
+        return ways[n];
+    }
+    public static void main(String[] args) {
+        int n = 45;
+        System.out.println("Solution Tabulation");
+        SolutionTabulation soltab = new SolutionTabulation();
+        System.out.println(soltab.climbStairs(n));
+    }
+}

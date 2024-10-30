cb61dd4473a51c31a5e2673d2ecd553f5077c899
dipanjal <dipanjalmaitra@gmail.com>
2024-10-30 12:56:26 +0600

    LC-70: Climbing Stair (Java memoization soln)

---
+++ leetcode/dp/Climbing_Stairs/java/SolutionMemo.java
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/climbing-stairs/description/
+// 70. Climbing Stairs
+
+import java.util.HashMap;
+import java.util.Map;
+
+// Recursive + Memoization | Top-Down | Dynamic Programming
+// Time Complexity: O(n) because we are using a memoization array to store the results of the subproblems
+// Space Complexity: O(n) because we are using a memoization array to store the results of the subproblems
+class SolutionMemo {
+    // memoization map to store the results of the subproblems
+    Map<Integer, Integer> memo;
+    public SolutionMemo() {
+        this.memo = new HashMap<>();
+        // adding base cases to the memoization map
+        this.memo.put(1, 1);
+        this.memo.put(2, 2);
+    }
+    public int climbStairs(int n) {
+        if (this.memo.containsKey(n)) {
+            return this.memo.get(n);
+        } else {
+            int res = climbStairs(n-2) + climbStairs(n-1);
+            this.memo.put(n, res);
+            return res;
+        }
+    }
+    public static void main(String[] args) {
+          int n = 45;
+        System.out.println("Solution Memo");
+        SolutionMemo solmemo = new SolutionMemo();
+        System.out.println(solmemo.climbStairs(n));
+    }
+}

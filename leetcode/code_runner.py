ab9ed12408a8cb1f4a47ecb72b1dd25f41568d9d
dipanjal <dipanjalmaitra@gmail.com>
2024-10-31 19:20:19 +0600

    runner refactoring + perf logging

---
+++ leetcode/code_runner.py
@@ -0,0 +1,183 @@
+# runner to run all the java files in the given directory
+
+import os, argparse, logging
+from enum import Enum
+from utils import log_execution_time
+
+logger = logging.getLogger(__name__)
+
+class CodeExtension(Enum):
+    JAVA = ".java"
+    PYTHON = ".py"
+
+class CodeType(Enum):
+    JAVA = "java"
+    PYTHON = "python"
+
+class CodeRunner:
+    def __init__(self, source_directory_abs: str, debug: bool=False, extension: CodeExtension=None):
+        self.source_directory_abs = source_directory_abs
+        self.DEBUG = debug
+        logging.basicConfig(
+            level=logging.DEBUG if self.DEBUG else logging.INFO
+        )
+        self.extension = extension
+    
+    def run(self, executable_file):
+        pass
+    
+    def process_files(self):
+        pass
+
+class PythonRunner(CodeRunner):
+    def __init__(self, source_directory_abs, debug=False):
+        super().__init__(
+            source_directory_abs, 
+            debug,
+            extension=CodeExtension.PYTHON
+        )
+
+    @log_execution_time
+    def run(self, executable_file):
+        # check if the class file does not exist
+        if not os.path.exists(f"{executable_file}"):
+            raise FileNotFoundError(f"Python file {executable_file} does not exist")
+        logger.info(f"Running {executable_file}")
+        os.system(f"python {executable_file}")
+
+
+    def process_files(self):
+        for file in os.listdir(self.source_directory_abs):
+            if file.endswith(self.extension.value):
+                self.run(file)
+                print("--------------------------------")
+    
+
+class JavaRunner(CodeRunner):
+    def __init__(self, source_directory_abs, debug=False):
+        super().__init__(
+            source_directory_abs, 
+            debug,
+            extension=CodeExtension.JAVA
+        )
+
+    def _compile_java(self, file):
+        class_name = file.split('.')[0]
+        # check if the java file does not exist
+        if not os.path.exists(file):
+            raise FileNotFoundError(f"File {file} does not exist")
+        logger.debug(f"Compiling {file}")
+        os.system(f"javac {file}")
+        return class_name
+
+
+    def _clean_up(self, class_name):
+        logger.debug(f"Cleaning up {class_name}")
+        os.system(f"rm -f {class_name}.class")
+
+
+    @log_execution_time
+    def run(self, executable_file):
+        # check if the class file does not exist
+        if not os.path.exists(f"{executable_file}.class"):
+            raise FileNotFoundError(f"Class file {executable_file}.class does not exist")
+        logger.info(f"Running {executable_file}")
+        os.system(f"java {executable_file}")
+
+
+    def process_files(self):
+        for file in os.listdir(self.source_directory_abs):
+            if file.endswith(self.type.value):
+                class_name = self._compile_java(file)
+                self.run(class_name)
+                self._clean_up(class_name)
+                print("--------------------------------")
+
+
+class Validator:
+    @classmethod
+    def exists(cls, path: str) -> bool:
+        prefix = "Directory" if os.path.isdir(path) else "File"
+        if not os.path.exists(path):
+            raise FileNotFoundError(f"{prefix} {path} does not exist")
+            
+    @classmethod
+    def contains_file(cls, source_dir: str, file_extention: str) -> bool:
+        # check if the directory contains any java files
+        if not any(file.endswith(file_extention) for file in os.listdir(source_dir)):
+            raise FileNotFoundError(f"Directory {source_dir} does not contain any {file_extention} files")
+
+class RunnerFactory:
+    @classmethod
+    def get_runner(cls, code_type: str):
+        logger.info(f"Code type: {code_type}")
+        if code_type == CodeType.JAVA.value:
+            return JavaRunner
+        if code_type == CodeType.PYTHON.value:
+            return PythonRunner
+        raise ValueError(f"Invalid code type: {code_type}")
+
+
+class Main:
+    @classmethod
+    def parse_args(cls):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--type', type=str, required=True, help='Type of code to run')
+        parser.add_argument('--path', type=str, required=True, help='Path to java files directory')
+        parser.add_argument('--debug', type=bool, required=False, help='enable debug mode')
+        args = parser.parse_args()
+        return vars(args)
+    
+    @classmethod
+    def process_directory(cls, runner: CodeRunner):
+        # check if the directory exists
+        Validator.exists(runner.source_directory_abs)
+        Validator.contains_file(runner.source_directory_abs, runner.extension.value)
+        
+        # change the working directory to the java directory
+        os.chdir(runner.source_directory_abs)
+        logger.debug(f"Current directory: {os.getcwd()}")
+        try:
+            runner.process_files()
+        except Exception as e:
+            logger.error(f"Error: {e}")
+
+
+    # if the given directory does not contain any java files, 
+    # search for the suggestive paths inside the given directory
+    @classmethod
+    def execute_with_suggestive_paths(cls, runner: CodeRunner, suggestive_paths: list[str]):
+        s_dir_abs = runner.source_directory_abs
+        counter = 0
+        while counter < len(suggestive_paths):
+            try:
+                cls.process_directory(runner)
+                break
+            except FileNotFoundError as e:
+                logger.error(f"Error: {e}")
+                logger.info(f"Searching for /{suggestive_paths[counter]} nested directory")
+                runner.source_directory_abs = os.path.join(s_dir_abs, suggestive_paths[counter])
+                counter += 1
+            except Exception as e:
+                logger.error(f"Error: {e}")
+                raise e
+
+    @classmethod
+    def main(cls, source_directory_abs: str, DEBUG: bool):
+        runner = RunnerFactory.get_runner(cli_args['type'])(source_directory_abs, DEBUG)
+        logger.info(f"Runner: {runner}")
+        suggestive_paths = [
+            "java", "Java",
+            "javaCode", "JavaCode", "java_code",
+            "JavaSolution", "javaSolution", "java_solution"
+        ]
+        cls.execute_with_suggestive_paths(runner, suggestive_paths)
+
+
+if __name__ == "__main__":
+    cli_args = Main.parse_args()
+    # convert the path to absolute path
+    source_directory_abs = os.path.abspath(cli_args['path'])
+    # enable debug mode if the debug flag is set
+    debug_mode = cli_args.get('debug', False)
+    Main.main(source_directory_abs, debug_mode)
---
+++ leetcode/utils.py
@@ -0,0 +1,27 @@
+import timeit
+from functools import wraps
+
+def log_execution_time(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        # classified_function_name = f"{args[0].__class__.__name__}.{func.__name__}"
+        print(f"-------- Executing {func.__name__} --------")
+        start = timeit.default_timer()
+        result = func(*args, **kwargs)
+        elapsed_time_ms = (timeit.default_timer() - start) * 1000
+        
+        def format_execution_time(elapsed_time_ms):
+            if elapsed_time_ms >= 1000:
+                elapsed_time_sec = elapsed_time_ms / 1000
+                if elapsed_time_sec >= 60:
+                    elapsed_time_min = elapsed_time_sec / 60
+                    return f"{elapsed_time_min:.2f} minutes"
+                else:
+                    return f"{elapsed_time_sec:.2f} seconds"
+            else:
+                return f"{elapsed_time_ms:.3f} ms"
+        
+        formatted_time = format_execution_time(elapsed_time_ms)
+        print(f"{func.__name__} executed in {formatted_time}")
+        return result
+    return wrapper
